46ea61cafa1d7262e4d12a22e81e570963454de5
Waranai <waflyaswarenyem@mail.ru>
2025-02-14 10:35:06 +0300

    made changes that Anastasia Pavlovna told me to do :)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,8 +24,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
+    }
+    @Override
+    protected  void onStart(){
+        super.onStart();
+        Log.d("Instens", "onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected  void onResume(){
+        super.onResume();
+        Log.d("Instens", "onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        Log.d("Instens", "onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle autState){
+        super.onSaveInstanceState(autState);
+        autState.putInt("Klic", count);
+        Log.d("Instens","onSave");
+        Toast.makeText(this, "onSave",Toast.LENGTH_LONG).show();
     }
 public void onClickButton(View view){
     count = 0;
